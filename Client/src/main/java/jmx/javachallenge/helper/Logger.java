5d84bc4891efe4805327043080235514b5f754e4
pjozsef <pollak.jozsef1@gmail.com>
2015-11-17 21:55:52 +0100

    Added json conversion to Logger.java

--- Client/src/main/java/jmx/javachallenge/helper/Logger.java
+++ Client/src/main/java/jmx/javachallenge/helper/Logger.java
@@ -1,18 +1,26 @@
 package jmx.javachallenge.helper;
 
+import com.google.gson.Gson;
+
 /**
  * Created by megam on 2015. 11. 15..
  */
 public class Logger {
     private static boolean enabled = false;
+    private static Gson gson = new Gson();
 
     public static void log(String message) {
-        if (Logger.enabled)
-            System.out.println(message);
+        if (Logger.enabled) log(new Message(message));
     }
 
     public static void log(Object message) {
-        if (Logger.enabled)
-            System.out.println(message.toString());
+        if (Logger.enabled) System.out.println("##START##" + gson.toJson(message) + "##END##");
+    }
+
+    private static class Message {
+        private String message;
+        public Message(String s) {
+            this.message = s;
+        }
     }
 }
