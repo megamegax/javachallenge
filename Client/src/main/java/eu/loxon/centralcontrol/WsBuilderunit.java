af1c1e309d8f0b5686c2c0db34eeef95e8c74d42
Hunyady Mihály <megamegax@gmail.com>
2015-11-18 19:15:15 +0100

    First turn is hardcoded, then control is passed to the builders

--- Client/src/main/java/eu/loxon/centralcontrol/WsBuilderunit.java
+++ Client/src/main/java/eu/loxon/centralcontrol/WsBuilderunit.java
@@ -1,19 +1,23 @@
 
 package eu.loxon.centralcontrol;
 
+import jmx.javachallenge.helper.Logger;
+import jmx.javachallenge.helper.Step;
+import jmx.javachallenge.helper.Util;
 import jmx.javachallenge.service.Service;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
+import java.util.Random;
 
 
 /**
  * <p>Java class for wsBuilderunit complex type.
- * 
+ * <p>
  * <p>The following schema fragment specifies the expected content contained within this class.
- * 
+ * <p>
  * <pre>
  * &lt;complexType name="wsBuilderunit">
  *   &lt;complexContent>
@@ -26,32 +30,29 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;/complexContent>
  * &lt;/complexType>
  * </pre>
- * 
- * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "wsBuilderunit", propOrder = {
-    "cord",
-    "unitid"
+        "cord",
+        "unitid"
 })
 public class WsBuilderunit {
 
     @XmlElement(required = true)
     protected WsCoordinate cord;
     protected int unitid;
-    protected boolean ownWill;
+    protected boolean ownWill = false;
     private Service service;
 
-    public WsBuilderunit(){
+    public WsBuilderunit() {
         this.service = Service.getInstance();
     }
+
     /**
      * Gets the value of the cord property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link WsCoordinate }
-     *     
+     *
+     * @return possible object is
+     * {@link WsCoordinate }
      */
     public WsCoordinate getCord() {
         return cord;
@@ -59,11 +60,9 @@ public class WsBuilderunit {
 
     /**
      * Sets the value of the cord property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link WsCoordinate }
-     *     
+     *
+     * @param value allowed object is
+     *              {@link WsCoordinate }
      */
     public void setCord(WsCoordinate value) {
         this.cord = value;
@@ -71,7 +70,6 @@ public class WsBuilderunit {
 
     /**
      * Gets the value of the unitid property.
-     * 
      */
     public int getUnitid() {
         return unitid;
@@ -79,7 +77,6 @@ public class WsBuilderunit {
 
     /**
      * Sets the value of the unitid property.
-     * 
      */
     public void setUnitid(int value) {
         this.unitid = value;
@@ -92,13 +89,66 @@ public class WsBuilderunit {
                 ", unitid=" + unitid +
                 '}';
     }
-    public boolean hasOwnWill(){
+
+    public boolean hasOwnWill() {
         return ownWill;
     }
-    public void setOwnWill(boolean will){
+
+    public void setOwnWill(boolean will) {
         this.ownWill = will;
     }
 
-    public void step(){
+    public void step() {
+        Logger.log(unitid + ", has own will");
+        //TODO építkezni, mozogni, nem visszalépni, figyelni mi merre van hajaj Marci alkoss valamit :D
+        WsDirection direction = moveRandomly();
+        service.watch(unitid);
+        if (doMove(unitid, direction)) {
+            doMove(unitid, direction);
+        }
     }
+
+    private boolean doMove(int unitID, WsDirection direction) {
+        WsCoordinate simulatedCoordinate = Util.simulateMove(service.builderUnits.get(unitID), direction);
+        Step step = Util.checkMovement(simulatedCoordinate);
+        Logger.log(step);
+        switch (step) {
+            case BUILD:
+                return service.structureTunnel(unitID, direction);
+
+            case MOVE:
+                return service.moveUnit(unitID, direction);
+
+            case WATCH:
+                return service.watch(unitID);
+
+            case EXPLODE:
+                return service.explode(unitID, direction);
+
+            case STAY:
+                return doMove(unitID, moveRandomly());
+
+            case NO_POINTS:
+                Logger.log("Elfogytak az elkölthető pontok");
+                return false;
+        }
+        return false;
+    }
+
+    private WsDirection moveRandomly() {
+        Random random = new Random();
+        int r = random.nextInt(6) + 1;
+        if (r == 1) {
+            return WsDirection.RIGHT;
+        } else if (r == 5) {
+            return WsDirection.RIGHT;
+        } else if (r == 2) {
+            return WsDirection.LEFT;
+        } else if (r == 3) {
+            return WsDirection.UP;
+        } else if (r == 6) {
+            return WsDirection.DOWN;
+        } else return WsDirection.DOWN;
+    }
+
 }
--- Client/src/main/java/jmx/javachallenge/client/Client.java
+++ Client/src/main/java/jmx/javachallenge/client/Client.java
@@ -37,69 +37,43 @@ public class Client {
         WsBuilderunit builder = service.selectedBuilder;
         int unitID = builder.getUnitid();
         //  int unitID = chooseBuilder();
-        if (isUnitInSpaceComp(unitID)) {
-            Logger.log(unitID + ", is in space comp");
-            service.watch(unitID);
-
-            if (unitID == 0) {
-                service.structureTunnel(unitID, moveOutFromSpaceComp());
-            }
-            service.moveUnit(unitID, moveOutFromSpaceComp());
-            service.watch(unitID);
-            //  doMove(unitID, moveOutFromSpaceComp());
+        if (builder.hasOwnWill()) {
+            builder.step();
         } else {
-            Logger.log(unitID + ", is NOT in space comp");
-            //TODO építkezni, mozogni, nem visszalépni, figyelni mi merre van hajaj Marci alkoss valamit :D
-            WsDirection direction = moveRandomly();
-            service.watch(unitID);
-            if (doMove(unitID, direction)) {
-                doMove(unitID, direction);
+            if (unitID == 0 && service.turnLeft == 70) {
+                service.watch(unitID);
+                service.structureTunnel(unitID, moveOutFromSpaceComp());
+                //TODO maradék pontból radarozni?
+            } else if (unitID == 1 && service.turnLeft == 70) {
+                service.moveUnit(unitID, moveOutFromSpaceComp());
+                WsDirection direction = WsDirection.DOWN;
+                service.structureTunnel(unitID, direction);
+                service.moveUnit(unitID, direction);
+                service.watch(unitID);
+                builder.setOwnWill(true);
+            } else if (unitID == 2 && service.turnLeft == 70) {
+                service.moveUnit(unitID, moveOutFromSpaceComp());
+                WsDirection direction = WsDirection.UP;
+                service.structureTunnel(unitID, direction);
+                service.moveUnit(unitID, direction);
+                service.watch(unitID);
+                builder.setOwnWill(true);
+            } else if (unitID == 3 && service.turnLeft == 70) {
+                service.moveUnit(unitID, moveOutFromSpaceComp());
+                WsDirection direction = WsDirection.RIGHT;
+                service.structureTunnel(unitID, direction);
+                service.moveUnit(unitID, direction);
+                service.watch(unitID);
+                builder.setOwnWill(true);
+            } else if (unitID == 0 && service.turnLeft == 69) {
+                service.moveUnit(unitID, moveOutFromSpaceComp());
+                builder.setOwnWill(true);
             }
         }
     }
 
-    private boolean doMove(int unitID, WsDirection direction) {
-        WsCoordinate simulatedCoordinate = Util.simulateMove(service.builderUnits.get(unitID), direction);
-        Step step = Util.checkMovement(simulatedCoordinate);
-        Logger.log(step);
-        switch (step) {
-            case BUILD:
-                return service.structureTunnel(unitID, direction);
 
-            case MOVE:
-                return service.moveUnit(unitID, direction);
 
-            case WATCH:
-                return service.watch(unitID);
-
-            case EXPLODE:
-                return service.explode(unitID, direction);
-
-            case STAY:
-                return doMove(unitID, moveRandomly());
-
-            case NO_POINTS:
-                Logger.log("Elfogytak az elkölthető pontok");
-                return false;
-        }
-        return false;
-    }
-
-    private WsDirection moveRandomly() {
-        Random random = new Random();
-        int r = random.nextInt(6) + 1;
-        if (r == 1) {
-            return WsDirection.RIGHT;
-        } else if (r == 5) {
-            return WsDirection.RIGHT;
-        } else if (r == 2) {
-            return WsDirection.LEFT;
-        } else if (r == 3) {
-            return WsDirection.UP;
-        } else if (r == 6) {
-            return WsDirection.DOWN;
-        } else return WsDirection.DOWN;
-    }
 
     private List<WsCoordinate> radarAround(int unitID) {
         List<WsCoordinate> coordinates = new ArrayList<>();
